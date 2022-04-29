2a2d79e5b6395db1aecdb617aad7be259dcf05a8
1Harvest <91007468+1Harvest@users.noreply.github.com>
2022-04-29 20:45:28 +0800

    Update sub_info.js

--- sgmodule/sub_info.js
+++ sgmodule/sub_info.js
@@ -39,7 +39,7 @@ Sub_info = script-name=Sub_info,update-interval=86400
   let total = info.total;
   let expire = args.expire || info.expire;
   let proportion = used / total;
-  let content = [`𝗨𝘀𝗮𝗴𝗲 : ${toPercent(proportion)} | 𝗕𝗮𝗹𝗮𝗻𝗰𝗲 : ${bytesToSize(total-used)}`];
+  let content = [`𝗨𝘀𝗮𝗴𝗲 : ${toPercent(proportion)} | 𝗕𝗮𝗹 : ${bytesToSize(total-used)}`];
 
 /*
   if (resetDayLeft) {
